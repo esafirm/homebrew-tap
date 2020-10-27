ddbbd9a67a84887db92c2262cdfcb26ad8aed553
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-27 09:33:54 +0700

    Brew formula update for gadb version 1.2.1

--- Formula/gadb.rb
+++ Formula/gadb.rb
@@ -2,16 +2,16 @@
 class Gadb < Formula
   desc "Golang ADB wrapper providing all ADB features and more"
   homepage "https://github.com/esafirm/homebrew-tap"
-  version "1.2.0"
+  version "1.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/esafirm/gadb/releases/download/1.2.0/gadb_1.2.0_Darwin_x86_64.tar.gz"
-    sha256 "0a7ddabf60602f8fbcb3e81a27522a537c48ee796a641649f8ed88372c19f8c5"
+    url "https://github.com/esafirm/gadb/releases/download/1.2.1/gadb_1.2.1_Darwin_x86_64.tar.gz"
+    sha256 "56fb381133f75a8429a1dccb8e629239f6b8de93da2d12480bd5c72859bd94a5"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/esafirm/gadb/releases/download/1.2.0/gadb_1.2.0_Linux_x86_64.tar.gz"
-      sha256 "fda5dd553058b400a7a29ac8a27b2305c8d7ea29d03bf734ee540fd6c03cac83"
+      url "https://github.com/esafirm/gadb/releases/download/1.2.1/gadb_1.2.1_Linux_x86_64.tar.gz"
+      sha256 "8ace21fe0d05edb372598dbb811bd2aa140a7fca6a82458dcadc2c809dc766e1"
     end
   end
 
